36d1cb4e390feefb3d566aed98fddf6a0f540304
AACooper1 <alexisacooper@gmail.com>
2026-06-16 19:34:35 -0600

    Add project files.

---
+++ N64Mus.UI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace N64Mus.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
